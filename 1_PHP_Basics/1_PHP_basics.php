1ad188a36462b5d8436e5d2fa505c92a2b3035a2
jonrhymes <jonrhymes@gmail.com>
2021-03-22 16:26:10 -0400

    learning php

---
+++ 1_PHP_Basics/1_PHP_basics.php
@@ -0,0 +1,8 @@
+/* PHP stands for Hypertext Preprocessor
+
+1. Access actionable info typed into a web form
+2. Find out browser and OS
+3. Display alternate web pages like mobile
+4. Create global HTML templates
+
+*/
