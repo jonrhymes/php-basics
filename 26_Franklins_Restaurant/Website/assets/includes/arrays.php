1c7bf7c6a6808977530aacfdf897741de6267dd4
jonrhymes <jonrhymes@gmail.com>
2021-03-29 18:06:04 -0400

    menu items added dynamically

--- 26_Franklins_Restaurant/Website/assets/includes/arrays.php
+++ 26_Franklins_Restaurant/Website/assets/includes/arrays.php
@@ -49,5 +49,35 @@
                             'img'   => 'carlos'
                         ),
 
-                    )
+                    );
+
+    // Menu Items
+
+    $menuItems = array(
+
+                    'club-sandwich' => array(
+                        'title' => 'Club Sandwich',
+                        'price' => 11.00,
+                        'blurb' => 'Spicy jalapeno bacon ipsum dolor amet cupidatat shankle ad chicken, officia id lorem fatback leberkas sirloin ball tip meatball quis andouille. Cillum kevin cupim tongue, aliquip salami pork belly jerky elit proident ham hock. Magna fugiat cow pariatur velit sausage veniam. Excepteur bacon rump velit exercitation. Laboris pork ham ham hock in sint. Drumstick porchetta alcatra tempor. Drumstick do anim proident.',
+                        'drink' => 'Club Soda'
+                    ),
+                    'dill-salmon' => array(
+                        'title' => 'Lemon &amp; Dill Salmon',
+                        'price' => 18.00,
+                        'blurb' => 'T-bone cupim short ribs frankfurter, ball tip ground round rump shankle pork pork chop jerky fatback pig doner brisket. Chicken picanha jowl, meatball strip steak alcatra tongue jerky burgdoggen pork loin kielbasa biltong fatback tenderloin filet mignon. Andouille tenderloin ham hock alcatra sausage jerky beef ribs. Tail andouille jerky landjaeger jowl. Flank bacon short loin t-bone beef brisket, boudin doner swine pig burgdoggen filet mignon kevin buffalo. Meatball filet mignon strip steak pork belly tenderloin.',
+                        'drink' => 'Pinot Grigio'
+                    ),
+                    'super-salad' => array(
+                        'title' => 'The Super Salad<sup>&reg;</sup>',
+                        'price' => 9.50,
+                        'blurb' => 'Beef ribs boudin strip steak, frankfurter burgdoggen chuck meatball. Pork belly leberkas tri-tip, chislic jowl venison ham ham hock tenderloin bacon hamburger. Chuck salami picanha prosciutto pig porchetta bresaola ribeye alcatra cow shankle. Ball tip t-bone pork belly, tail jowl shank sausage. Turducken pork chop jowl corned beef pork loin swine.',
+                        'drink' => 'Pinot Noir &amp; Fernet'
+                    ),
+                    'mexican-barbacoa' => array(
+                        'title' => 'Mexican Barbacoa',
+                        'price' => 34.00,
+                        'blurb' => 'Pork belly cow chicken bacon tongue. T-bone chicken andouille turkey sausage picanha fatback frankfurter pancetta kevin filet mignon. Picanha cow landjaeger jerky, burgdoggen ribeye spare ribs tri-tip brisket. Tail kevin andouille, kielbasa ground round pancetta turducken ribeye tri-tip hamburger. Doner short loin rump, venison pork loin pancetta ham filet mignon swine shank chuck boudin andouille meatball meatloaf.',
+                        'drink' => 'Beer &amp; a shot'
+                    ),
+                );
 ?>
